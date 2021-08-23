753bdaf776384cde72f5b9e56511173bb953bfd6
Ego <augurto.17@gmail.com>
2021-08-23 08:58:50 -0500

    AGEGANDO ENTREGABLES AL MODAL DE EDTIDAR PROYECTO

--- modal/editar_proyecto.php
+++ modal/editar_proyecto.php
@@ -24,11 +24,18 @@
           <input type="text" class="form-control" id="mod_codigo" name="mod_codigo">
         </div>
         </div>
-         <div class="form-group">
-        <label for="mod_telefono" class="col-sm-3 control-label">Presupuesto</label>
-        <div class="col-sm-12">
-          <input type="text" class="form-control" id="mod_presupuesto" name="mod_presupuesto">
-        </div>
+        <div class="form-group">
+            <label for="mod_telefono" class="col-sm-3 control-label">Presupuesto</label>
+            <div class="col-sm-12">
+              <input type="text" class="form-control" id="mod_presupuesto" name="mod_presupuesto">
+            </div>
+
+
+            <div class="form-group">
+            <label for="mod_telefono" class="col-sm-3 control-label">Entregables</label>
+            <div class="col-sm-12">
+              <input type="text" class="form-control" id="mod_presupuesto" name="mod_presupuesto">
+            </div>
         </div>
          <div class="form-group">
         <label for="mod_descripcion" class="col-sm-3 control-label">Descripcion</label>
@@ -36,20 +43,55 @@
           <textarea class="form-control" id="mod_descripcion" name="mod_descripcion"></textarea>
         </div>
         </div>
-         <div class="form-group">
-        <label for="mod_telefono" class="col-sm-3 control-label">Estado</label>
-        <div class="col-sm-12">
-          <select name="mod_estado"  id="mod_estado"  class="form-control">
-          <option value="activo">Activo</option>
-          <option value="inactivo">Inactivo</option>
-          <option value="terminado">Terminado</option>
-          </select>
-        </div>
+              <div class="form-group">
+              <label for="mod_telefono" class="col-sm-3 control-label">Estado</label>
+              <div class="col-sm-12">
+                <select name="mod_estado"  id="mod_estado"  class="form-control">
+                <option value="activo">Activo</option>
+                <option value="inactivo">Inactivo</option>
+                <option value="terminado">Terminado</option>
+                </select>
+              </div>
         </div>
         <div class="modal-footer">
           <button class="btn btn-secondary" type="button" data-dismiss="modal">Cancelar</button>
             <button type="submit" class="btn btn-primary" id="actualizar_datos">Actualizar</button>
         </div>
+        
+
+<!--         agregando los entregables  -->
+
+          <div class="container">
+            <p></p>
+                <a class="btn btn-primary" href="javascript:void(0)" id="addInput">
+                  <span class="glyphicon glyphicon-plus" aria-hidden="true"></span>
+                  Mas entregables
+                </a>
+                <br>
+              <label>Entregables</label>
+                <div id="dynamicDiv">
+                      <div class="row">
+                      <div class="col-lg-6">
+                        <div class="input-group">
+                          <div class="input-group-prepend">
+                          <span class="input-group-text">
+                          <span class="fa fa-file">
+                          </span>
+                        </span>
+                        </div><input type="text" class="form-control" name="entregable[]" onkeyup="loa();" placeholder="Nombre del entregable" required>
+                        </div>
+                        </div>
+                          <div class="col-lg-5"><div class="input-group"><div class="input-group-prepend"><span class="input-group-text"><span class="fas fa-calendar"></span></span></div><input type="date" class="form-control"  name="f[]" onkeyup="loa();" placeholder="Codigo" required></div></div></div><p></p>
+                        </div>
+                
+                </div>
+             
+                </form>
+                </div>
+              </div>
+            </div>
+          </div>
+<!-- aca termina el modal de agregar los entregables -->
       </form>
       </div>
     </div>
