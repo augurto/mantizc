9a49d1aa52689c6e3619a40e78614c15e6be888f
Ego <augurto.17@gmail.com>
2021-08-24 07:21:30 -0500

    IGUALANDO A 11

--- ajax/buscar_miembros.php
+++ ajax/buscar_miembros.php
@@ -30,7 +30,7 @@ session_start();
 	}
 	if($action == 'ajax'){
 		$id_e=$_POST['id_p'];
-		$sql="SELECT * FROM  entregables WHERE codigo_proyecto='".$id_e."'";
+		$sql="SELECT * FROM  entregables WHERE codigo_proyecto='11'";
 		$query = mysqli_query($con, $sql);
 			?>
 			<div class="card shadow mb-4">
@@ -42,7 +42,7 @@ session_start();
                 <table class="table table-bordered" id="dataTable" width="100%" cellspacing="0">
                   <thead>
                     <tr>
-                    <th>Codigo_proyectoo</th>
+                    <th>Codigo_proyecto</th>
 					<th>Nombre</th>
 					<th>Fecha de entrega</th>
 					
