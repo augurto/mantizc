a64e5a17c5d7a3d8ff12da95262c42ddac31351f
Ego <augurto.17@gmail.com>
2021-08-28 07:35:08 -0500

    cambio de posicion

--- info_proyecto.php
+++ info_proyecto.php
@@ -169,9 +169,16 @@ require_once ("./config/db.php");//Contiene las variables de configuracion para 
           </div>
                   <!-- moviendo toda la vaina  -->
 
-<br>
+
+
+<!--               aca termina  -->
+        </div>
+        <!-- /.container-fluid -->
+
+
+
                      <!-- Earnings (Monthly) Card Example -->
-            <div class="col-xl-3 col-md-6 mb-4">
+                     <div class="col-xl-3 col-md-6 mb-4">
               <div class="card border-left-primary shadow h-100 py-2">
                 <div class="card-body">
                   <div class="row no-gutters align-items-center">
@@ -246,9 +253,7 @@ require_once ("./config/db.php");//Contiene las variables de configuracion para 
               </div>
               </div>
 
-<!--               aca termina  -->
-        </div>
-        <!-- /.container-fluid -->
+
 
       </div>
       <!-- End of Main Content -->
